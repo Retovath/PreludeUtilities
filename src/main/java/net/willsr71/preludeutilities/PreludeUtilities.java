5b8372dcaea1642a66510416208470cbd9e340b2
Willsr71 <willsr71@gmail.com>
2015-11-28 19:28:35 -0500

    Initial Commit #4

--- src/main/java/net/willsr71/preludeutilities/PreludeUtilities.java
+++ src/main/java/net/willsr71/preludeutilities/PreludeUtilities.java
@@ -1,4 +1,16 @@
 package net.willsr71.preludeutilities;
 
-public class PreludeUtilities {
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class PreludeUtilities extends JavaPlugin {
+    public static PreludeUtilities instance;
+    public static String version = "1.0-DEV";
+
+    public void onEnable() {
+        instance = this;
+    }
+
+    public void onDisable() {
+
+    }
 }
